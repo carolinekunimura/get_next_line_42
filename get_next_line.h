fbe21b373a4a565b7b27d21f11d4d58865d7c46c
Caroline Kunimura Plaza <ckunimur@c1r6p4.42sp.org.br>
2022-10-12 16:53:10 -0300

    first

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 14:14:50 by ckunimur          #+#    #+#             */
+/*   Updated: 2022/10/12 16:45:35 by ckunimur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+// functions 
+
+char	*get_next_line(int fd);
+#endif
